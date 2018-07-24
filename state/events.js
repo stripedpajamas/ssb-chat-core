c19d1cdaa01426bad834c3a82301bacdd1929ccb
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-07-24 14:24:22 -0400

    Add message event

--- state/events.js
+++ state/events.js
@@ -1,9 +1,14 @@
 let onModeChange = () => {}
+let onNewMessage = () => {}
 
 module.exports = {
   // cb receives new mode
   mode: {
     onChange: (cb) => { onModeChange = cb },
     _change: (mode) => onModeChange(mode)
+  },
+  messages: {
+    onNew: (cb) => { onNewMessage = cb },
+    _new: () => { onNewMessage() }
   }
 }
