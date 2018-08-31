0e5849e4e169aeb3cd9f5b3e1b49fed24c775438
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-08-30 22:56:36 -0400

    Update pnpm file

--- pnpmfile.js
+++ pnpmfile.js
@@ -1,14 +1,30 @@
 module.exports = {
   hooks: {
     readPackage: (pkg, context) => {
-      if (pkg.name === 'ssb-validate') {
-        const ssbKeysVersion = pkg.devDependencies['ssb-keys']
-        pkg.dependencies = {
-          ...pkg.dependencies,
-          'ssb-keys': ssbKeysVersion
+      switch (pkg.name) {
+        case 'ssb-validate': {
+          const ssbKeysVersion = pkg.devDependencies['ssb-keys']
+          pkg.dependencies = {
+            ...pkg.dependencies,
+            'ssb-keys': ssbKeysVersion
+          }
+          delete pkg.devDependencies['ssb-keys']
+          context.log(`Added ssb-keys@${ssbKeysVersion} to ssb-validate dependencies`)
+          break
         }
-        delete pkg.devDependencies['ssb-keys']
-        context.log(`Added ssb-keys@${ssbKeysVersion} to ssb-validate dependencies`)
+        case 'ssb-ws':
+        case 'ssb-names':
+        case 'ssb-meme': {
+          const pullStreamVersion = pkg.devDependencies['pull-stream'] || '^3.6.8'
+          pkg.dependencies = {
+            ...pkg.dependencies,
+            'pull-stream': pullStreamVersion
+          }
+          delete pkg.devDependencies['pull-stream']
+          context.log(`Added pull-stream@${pullStreamVersion} to ${pkg.name} dependencies`)
+        }
+        default:
+          break
       }
       return pkg
     }
