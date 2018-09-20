b030e74bebc540e43e3989e64dbcc365fa0ab14b
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-09-19 21:07:19 -0400

    Disable nanotiming

--- state/events.js
+++ state/events.js
@@ -1,3 +1,11 @@
+// we don't want weird perf hook stuff
+if (typeof process !== 'undefined') {
+  process.env['DISABLE_NANOTIMING'] = false
+}
+if (typeof window !== 'undefined' && window.localStorage) {
+  window.localStorage.DISABLE_NANOTIMING = true
+}
+
 const nanobus = require('nanobus')
 
 module.exports = nanobus()
